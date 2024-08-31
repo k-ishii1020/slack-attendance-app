baa846f7655c6593706302d48f25210cda2e9fa4
k-ishii1020 <40662914+8492GitKaz@users.noreply.github.com>
2024-09-01 05:51:12 +0900

    ロギング設定

---
+++ app/config/logging_config.py
@@ -0,0 +1,56 @@
+import logging
+import os
+from logging.handlers import TimedRotatingFileHandler
+
+from slack_log_handler import SlackLogHandler
+
+# ログフォーマットを設定
+LOG_FORMAT = "%(asctime)s %(levelname)s : %(message)s"
+
+EMOJIS = {
+    logging.NOTSET: ":loudspeaker:",
+    logging.DEBUG: ":speaker:",
+    logging.INFO: ":memo:",
+    logging.WARNING: ":warning:",
+    logging.ERROR: ":rotating_light:",
+    logging.CRITICAL: ":warning:",
+}
+
+
+def setup_logging():
+    """ロギング設定を行う"""
+    # ファイルハンドラーを作成
+    file_handler = TimedRotatingFileHandler("./logs/error.log", when="D", backupCount=30)
+    file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+
+    # コンソールハンドラーを作成
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    console_handler.setLevel(logging.INFO)
+
+    # Slack エラーハンドラーを作成
+    slack_error_handler = SlackLogHandler(
+        os.getenv("LOG_ERROR_WEBHOOK"),
+        username=os.getenv("NICKNAME"),
+        emojis=EMOJIS,
+    )
+    slack_error_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    slack_error_handler.setLevel(logging.ERROR)
+
+    # Slack 通知ハンドラーを作成
+    slack_info_handler = SlackLogHandler(
+        os.getenv("LOG_INFO_WEBHOOK"),
+        username=os.getenv("NICKNAME"),
+        emojis=EMOJIS,
+    )
+    slack_info_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    slack_info_handler.setLevel(logging.INFO)
+
+    # Loggerの基本設定
+    logging.basicConfig(
+        level=logging.INFO,
+        format=LOG_FORMAT,
+        handlers=[file_handler, console_handler],
+        # handlers=[file_handler, console_handler, slack_error_handler, slack_info_handler],
+    )
+    return logging.getLogger(__name__)
