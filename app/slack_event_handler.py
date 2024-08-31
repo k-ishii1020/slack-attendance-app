36dd5d843cde6f42e0684da7ff8d2f22eaee4798
k-ishii1020 <40662914+8492GitKaz@users.noreply.github.com>
2024-09-01 06:22:26 +0900

    リファクタ

--- app/slack_event_handler.py
+++ app/slack_event_handler.py
@@ -17,46 +17,48 @@ class SlackEventHandlers:
         @self.app.event("app_home_opened")
         def handle_app_home_opened(ack, body, client):
             ack()
-            user_id = body["event"]["user"]
-
-            # ホームタブのブロックを/app/config/app_home.jsonから読み込む
-            with open("app/blocks/app_home.json", "r") as f:
-                app_home_json = json.load(f)
-
-            # ユーザーのホームタブを更新
-            client.views_publish(user_id=user_id, view=app_home_json)
+            self.button_action(ack=ack, user_id=body["event"]["user"], client=client, notification_message="-")
 
         @self.app.action("begin_office_work")
         def handle_begin_office_work(ack, body, client):
-            self.button_action(ack=ack, body=body, client=client, notification_message="オフィス出勤を投稿しました")
+            ack()
+            self.button_action(
+                ack=ack, user_id=body["user"]["id"], client=client, notification_message="オフィス出勤を投稿しました"
+            )
 
         @self.app.action("begin_remote_work")
         def handle_begin_remote_work(ack, body, client):
-            self.button_action(ack=ack, body=body, client=client, notification_message="リモート出勤を投稿しました")
+            ack()
+            self.button_action(
+                ack=ack, user_id=body["user"]["id"], client=client, notification_message="リモート出勤を投稿しました"
+            )
 
         @self.app.action("finish_work")
         def handle_finish_work(ack, body, client):
+            ack()
             self.button_action(
                 ack=ack,
-                body=body,
+                user_id=body["user"]["id"],
                 client=client,
                 notification_message="退勤を投稿しました。今日も一日お疲れ様でした！",
             )
 
         @self.app.action("begin_break_time")
         def handle_begin_break_time(ack, body, client):
+            ack()
             self.button_action(
                 ack=ack,
-                body=body,
+                user_id=body["user"]["id"],
                 client=client,
                 notification_message="休憩開始を投稿しました。ゆっくり休んでくださいね！",
             )
 
         @self.app.action("finish_break_time")
         def handle_finish_break_time(ack, body, client):
+            ack()
             self.button_action(
                 ack=ack,
-                body=body,
+                user_id=body["user"]["id"],
                 client=client,
                 notification_message="休憩終了を投稿しました。後半戦も頑張りましょう！",
             )
@@ -70,15 +72,17 @@ class SlackEventHandlers:
 
         @self.app.view("submit_personal_settings")
         def handle_submit_personal_settings(ack, body, client):
-            self.button_action(ack=ack, body=body, client=client, notification_message="個人設定を保存しました")
+            ack()
+            self.button_action(
+                ack=ack, user_id=body["user"]["id"], client=client, notification_message="個人設定を保存しました"
+            )
 
     @staticmethod
-    def button_action(ack, body, client, notification_message):
-        ack()
+    def button_action(ack, user_id, client, notification_message):
         with open("app/blocks/app_home.json", "r") as f:
             app_home_json = json.load(f)
 
         app_home_json["blocks"][NOTIFICATION_JSON_NUMBER]["text"]["text"] = (
             f"アプリからのお知らせ: *{notification_message}*"
         )
-        client.views_publish(user_id=body["user"]["id"], view=app_home_json)
+        client.views_publish(user_id=user_id, view=app_home_json)
