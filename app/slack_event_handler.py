0edd2e50a122ced81b07f5cb8a5ddc15675fad3d
k-ishii1020 <40662914+8492GitKaz@users.noreply.github.com>
2024-09-02 00:16:47 +0900

    AppHome完成

--- app/slack_event_handler.py
+++ app/slack_event_handler.py
@@ -6,7 +6,7 @@ from slack_bolt import App
 
 from app.db_service import DBService
 
-NOTIFICATION_JSON_NUMBER = 10
+NOTIFICATION_JSON_NUMBER = 9
 
 
 class SlackEventHandlers:
@@ -88,6 +88,9 @@ class SlackEventHandlers:
             values = body["view"]["state"]["values"]
 
             # fmt: off
+            begin_office_work_message = values["begin_office_work_message"]["begin_office_work_message"]["value"]
+            begin_remote_work_message = values["begin_remote_work_message"]["begin_remote_work_message"]["value"]
+            finish_work_message = values["finish_work_message"]["finish_work_message"]["value"]
             attendance_channel_ids = values["attendance_channel_ids"]["attendance_channel_ids"]["selected_conversations"]
             attendance_thread_channel_id = values["attendance_thread_channel_id"]["attendance_thread_channel_id"]["selected_conversation"]
             attendance_thread_message =values["attendance_thread_message"]["attendance_thread_message"]["value"]
@@ -101,6 +104,9 @@ class SlackEventHandlers:
             )
             if result:
                 personal_settings_json_data = {
+                    "begin_office_work_message": begin_office_work_message,
+                    "begin_remote_work_message": begin_remote_work_message,
+                    "finish_work_message": finish_work_message,
                     "attendance_channel_ids": attendance_channel_ids,
                     "attendance_thread_channel_id": attendance_thread_channel_id,
                     "attendance_thread_message": attendance_thread_message,
@@ -118,7 +124,7 @@ class SlackEventHandlers:
             app_home_json = json.load(f)
 
         app_home_json["blocks"][NOTIFICATION_JSON_NUMBER]["text"]["text"] = (
-            f"アプリからのお知らせ: *{notification_message}*"
+            f"*アプリからのお知らせ*:\n {notification_message}"
         )
         client.views_publish(user_id=user_id, view=app_home_json)
 
@@ -127,14 +133,18 @@ class SlackEventHandlers:
         attendance_channel_ids, attendance_thread_channel_id, attendance_thread_message
     ) -> tuple:
         if not attendance_channel_ids and not attendance_thread_channel_id:
-            return False, ":warning:勤怠専用チャンネルか勤怠スレッドのどちらかは必須です"
+            return False, ":warning: チャンネルに直接投稿タイプかスレッド内投稿タイプのどちらかは必須です"
 
         if attendance_thread_channel_id and not attendance_thread_message:
-            return False, ":warning:勤怠スレッドチャンネルを設定する場合はスレッドの最初のメッセージを入力してください"
+            return False, ":warning:スレッド内投稿タイプを設定する場合はスレッドの最初のメッセージを入力してください"
         return True, "個人設定を保存しました"
 
     @staticmethod
     def create_personal_settings_view(personal_settings_data):
+        # デフォルト値
+        begin_office_work_message = "業務開始します（出社）"
+        begin_remote_work_message = "業務開始します（リモート）"
+        finish_work_message = "業務終了します。お疲れ様でした。"
         attendance_channel_ids = []
         attendance_thread_channel_id = None
         attendance_thread_message = ""
@@ -143,6 +153,9 @@ class SlackEventHandlers:
         change_profile_status_value = "True"
 
         if personal_settings_data:
+            begin_office_work_message = personal_settings_data["begin_office_work_message"]
+            begin_remote_work_message = personal_settings_data["begin_remote_work_message"]
+            finish_work_message = personal_settings_data["finish_work_message"]
             attendance_channel_ids = personal_settings_data["attendance_channel_ids"]
             attendance_thread_channel_id = personal_settings_data["attendance_thread_channel_id"]
             attendance_thread_message = personal_settings_data["attendance_thread_message"]
@@ -160,24 +173,89 @@ class SlackEventHandlers:
             "title": {"type": "plain_text", "text": "出退勤Botの個人設定", "emoji": True},
             "blocks": [
                 {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": ":warning: *勤怠専用チャンネル or 勤怠スレッドのどちらかは必須です*",
+                    "type": "header",
+                    "text": {"type": "plain_text", "text": ":memo: 投稿メッセージ設定", "emoji": True},
+                },
+                {
+                    "type": "input",
+                    "block_id": "begin_office_work_message",
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "begin_office_work_message",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "例: 業務開始します（出社）",
+                            "emoji": True,
+                        },
+                        "initial_value": begin_office_work_message,
                     },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "オフィス勤務時に投稿するメッセージを入力",
+                        "emoji": True,
+                    },
+                    "optional": True,
+                },
+                {
+                    "type": "input",
+                    "block_id": "begin_remote_work_message",
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "begin_remote_work_message",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "例: 業務開始します（リモート）",
+                            "emoji": True,
+                        },
+                        "initial_value": begin_remote_work_message,
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "リモート勤務時に投稿するメッセージを入力",
+                        "emoji": True,
+                    },
+                    "optional": True,
+                },
+                {
+                    "type": "input",
+                    "block_id": "finish_work_message",
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "finish_work_message",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "例: 業務終了します。お疲れ様でした。",
+                            "emoji": True,
+                        },
+                        "initial_value": finish_work_message,
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "リモート勤務時に投稿するメッセージを入力",
+                        "emoji": True,
+                    },
+                    "optional": True,
                 },
                 {"type": "divider"},
+                {
+                    "type": "header",
+                    "text": {"type": "plain_text", "text": ":mega: 投稿チャンネル設定", "emoji": True},
+                },
                 {
                     "type": "input",
                     "block_id": "attendance_channel_ids",
                     "element": {
                         "type": "multi_conversations_select",
                         "action_id": "attendance_channel_ids",
-                        "placeholder": {"type": "plain_text", "text": "チャンネルを選択", "emoji": True},
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "対象チャンネルを選択（複数選択可）",
+                            "emoji": True,
+                        },
                         "filter": {"include": ["public", "private"], "exclude_bot_users": True},
                         "initial_conversations": attendance_channel_ids,
                     },
-                    "label": {"type": "plain_text", "text": "勤怠専用チャンネル（複数選択可）", "emoji": True},
+                    "label": {"type": "plain_text", "text": "チャンネルに直接投稿タイプ", "emoji": True},
                     "optional": True,
                 },
                 {
@@ -197,13 +275,17 @@ class SlackEventHandlers:
                     "element": {
                         "type": "conversations_select",
                         "action_id": "attendance_thread_channel_id",
-                        "placeholder": {"type": "plain_text", "text": "チャンネルを選択", "emoji": True},
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "対象チャンネルを選択（複数選択不可）",
+                            "emoji": True,
+                        },
                         "filter": {"include": ["public", "private"], "exclude_bot_users": True},
                         "initial_conversation": attendance_thread_channel_id,
                     },
                     "label": {
                         "type": "plain_text",
-                        "text": "勤怠スレッドがあるチャンネルを選択（複数選択不可）",
+                        "text": "スレッド内投稿タイプ",
                         "emoji": True,
                     },
                     "optional": True,
@@ -223,10 +305,25 @@ class SlackEventHandlers:
                     },
                     "label": {
                         "type": "plain_text",
-                        "text": "スレッドの最初のメッセージを入力（前方一致）",
+                        "text": "対象スレッドのメッセージを入力（前方一致）",
                         "emoji": True,
                     },
                     "optional": True,
+                },
+                {
+                    "type": "context",
+                    "elements": [
+                        {
+                            "type": "image",
+                            "image_url": "https://api.slack.com/img/blocks/bkb_template_images/placeholder.png",
+                            "alt_text": "placeholder",
+                        }
+                    ],
+                },
+                {"type": "divider"},
+                {
+                    "type": "header",
+                    "text": {"type": "plain_text", "text": ":house: プロフィールステータス設定", "emoji": True},
                 },
                 {
                     "type": "input",
@@ -259,8 +356,11 @@ class SlackEventHandlers:
             ],
         }
 
-        # attendance_thread_channel_idがNoneの場合、"initial_conversation": attendance_thread_channel_idの行を削除
+        # 初期登録前の場合は不要な要素を削除
         if not attendance_thread_channel_id:
-            del views["blocks"][5]["element"]["initial_conversation"]
+            del views["blocks"][9]["element"]["initial_conversation"]
+
+        if not attendance_thread_message:
+            del views["blocks"][10]["element"]["initial_value"]
 
         return views
